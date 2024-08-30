528addfe43b1562baa5d56a1af25d8735c279bf0
Abhinav <149236922+Gitabhi69@users.noreply.github.com>
2024-08-30 14:51:54 +0545

    Update config_tmp.js

--- src/config/config_tmp.js
+++ src/config/config_tmp.js
@@ -1,4 +1,4 @@
 export class Config {
-  static TELEGRAM_APP_ID = undefined; // YOUR APP ID
-  static TELEGRAM_APP_HASH = undefined; // YOUR APP HASH
+  static TELEGRAM_APP_ID = 24678253; // YOUR APP ID
+  static TELEGRAM_APP_HASH = "c60cb246d88d3444ab81b7c41916645c"; // YOUR APP HASH
 }
